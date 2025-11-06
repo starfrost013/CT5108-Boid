5fd328340c32460ecfc9811e3a94ab61ab6051a9
starfrost013 <mario64crashed@gmail.com>
2025-11-06 22:24:24 +0000

    big refactor to make both of them inherit from a base class

--- Source/Boid/BoidObject.cpp
+++ Source/Boid/BoidObject.cpp
@@ -3,10 +3,10 @@
 // Copyright © 2025 starfrost 
 //
 
+#include "BoidBase.h"
 #include "BoidObject.h"
 #include "BoidManager.h"
 
-#define BOID_EPSILON_LENGTH			256.0f
 
 // Sets default values
 ABoidObject::ABoidObject()
@@ -16,94 +16,6 @@ ABoidObject::ABoidObject()
 
 	// create a sphere
 	mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BoidSphere"));
-}
-
-FVector ABoidObject::Steer(FVector goalPosition, BoidSteeringBehaviour steeringBehaviourType)
-{
-	FVector myLocation = GetActorLocation();
-	FVector newCurrentVelocity; 
-
-	switch (steeringBehaviourType)
-	{
-		case BoidSteeringBehaviour::Seek:
-			newCurrentVelocity = goalPosition - myLocation; 
-			break;
-		case BoidSteeringBehaviour::Flee:
-			newCurrentVelocity = myLocation - goalPosition;
-			break;
-		case BoidSteeringBehaviour::Pursue:
-			// try to use it
-			newCurrentVelocity = goalPosition; 
-			break;
-		case BoidSteeringBehaviour::Evade:
-			newCurrentVelocity = -goalPosition;
-			break;
-	}
-
-	newCurrentVelocity.Normalize();
-	return newCurrentVelocity;
-}
-
-FVector ABoidObject::Flock(TArray<ABoidObject*> neighbours, BoidFlockingBehaviour flockingBehaviourType)
-{
-	FVector currentFlockVel = FVector(), averageLocation = FVector();
-	uint32_t numNeighbours = neighbours.Num();
-
-	if (!numNeighbours)
-		return currentFlockVel;
-
-	switch (flockingBehaviourType)
-	{
-		case BoidFlockingBehaviour::Alignment:
-			for (ABoidObject* boid : neighbours)
-				currentFlockVel += boid->currentVelocity;
-
-			currentFlockVel /= numNeighbours;
-			break;
-		case BoidFlockingBehaviour::Cohere:
-			for (ABoidObject* boid : neighbours)
-				averageLocation += boid->GetActorLocation();
-
-			averageLocation /= numNeighbours;
-
-			currentFlockVel = Steer(averageLocation, BoidSteeringBehaviour::Seek);
-			// additional cohere steps
-			break;
-		case BoidFlockingBehaviour::Separate:
-			for (ABoidObject* boid : neighbours)
-				currentFlockVel += Steer(boid->GetActorLocation(), BoidSteeringBehaviour::Flee);
-
-			currentFlockVel /= numNeighbours;
-			break;
-	}
-	currentFlockVel.Normalize();
-
-	return currentFlockVel;
-}
-
-
-FVector ABoidObject::Wander()
-{
-	FVector position = GetActorLocation();
-
-	double dist = (position - currentWanderTarget).Size();
-
-	// the boid will spazz around constantly because if the new target is too close it may endlessly try to select new targets.
-	// let's store our old wander target, and wait until we're more than the epsilon away from THAT before picking a new one (as long as we're not also right next to the new one)
-
-	bool isFarEnoughAwayFromOld = ((position - oldWanderTarget).Size()) > BOID_EPSILON_LENGTH;
-
-	// pick a random target
-	if (currentWanderTarget == FVector::ZeroVector
-	|| FMath::Abs(dist) < BOID_EPSILON_LENGTH
-		&& isFarEnoughAwayFromOld)
-	{
-		oldWanderTarget = currentWanderTarget;
-		currentWanderTarget = (position + (FMath::VRand() * manager->WanderRadius));
-	}
-
-	FVector wanderVelocity = Steer(currentWanderTarget, BoidSteeringBehaviour::Seek);
-	return wanderVelocity;
 }
 
 // Squirrels don't have strong heads
@@ -136,16 +48,6 @@ FVector ABoidObject::Avoidance()
 		return FVector::ZeroVector;
 }
 
-void ABoidObject::SetPhysicsType()
-{
-	if (manager->PhysicsType != manager->PHYSICS_TYPE_NONE)
-	{
-		mesh->SetSimulatePhysics(true);
-		
-		if (manager->PhysicsType == manager->PHYSICS_TYPE_PHYS_NOGRAV)
-			mesh->SetEnableGravity(false);
-	}
-}
 
 // Called when the game starts or when spawned
 void ABoidObject::BeginPlay()
@@ -160,10 +62,10 @@ void ABoidObject::Tick(float deltaTime)
 
 	// Steer for this frame with deltatime applied
 
-	targetVelocity = Flock(manager->GetBoidsWithinRange(this, manager->BoidPerceptionRadius), BoidFlockingBehaviour::Separate) * manager->SeparationWeight;
-	targetVelocity += Flock(manager->GetBoidsWithinRange(this, manager->BoidPerceptionRadius), BoidFlockingBehaviour::Cohere) * manager->CohesionWeight;
-	targetVelocity += Flock(manager->GetBoidsWithinRange(this, manager->BoidPerceptionRadius), BoidFlockingBehaviour::Alignment) * manager->AlignmentWeight;
-	targetVelocity += Wander();
+	targetVelocity = Super::Flock<ABoidObject>(manager->GetBoidsWithinRange(this, manager->BoidPerceptionRadius), BoidFlockingBehaviour::Separate) * manager->SeparationWeight;
+	targetVelocity += Super::Flock<ABoidObject>(manager->GetBoidsWithinRange(this, manager->BoidPerceptionRadius), BoidFlockingBehaviour::Cohere) * manager->CohesionWeight;
+	targetVelocity += Super::Flock<ABoidObject>(manager->GetBoidsWithinRange(this, manager->BoidPerceptionRadius), BoidFlockingBehaviour::Alignment) * manager->AlignmentWeight;
+	targetVelocity += Super::Wander();
 
 	// try and steer away from any obstacles e.g. other boids
 
--- Source/Boid/BoidObject.h
+++ Source/Boid/BoidObject.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include "BoidBase.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "BoidObject.generated.h"
@@ -12,7 +13,7 @@
 class ABoidManager;
 
 UCLASS()
-class BOID_API ABoidObject : public AActor
+class BOID_API ABoidObject : public ABoidBase
 {
 	GENERATED_BODY()
 	
@@ -20,23 +21,6 @@ public:
 	// Sets default values for this actor's properties
 	ABoidObject();
 
-	enum BoidSteeringBehaviour
-	{
-		Seek = 0,							// Seek to a position.
-		Flee = 1,							// Flee from a position.
-		Pursue = 2,							// Pursue a position.
-		Evade = 3,							// Evade (i.e. anticipate and try to get away from) a position.
-		Arrive = 4,							// Try to find a position.
-		SteerToAvoid = 5,					// Steer away from something.
-		AvoidObstacles = 6,					// Avoid any obstacles.
-	};
-
-	enum BoidFlockingBehaviour
-	{
-		Alignment = 0,
-		Cohere = 1,
-		Separate = 2,
-	};
 
 	struct BoidWeights
 	{
@@ -48,11 +32,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// i am not sure this is a good ideas
-	FVector Steer(FVector startPosition, BoidSteeringBehaviour steeringBehaviourType);
-	FVector Flock(TArray<ABoidObject*> neighbours, BoidFlockingBehaviour flockingBehaviourType);
-	FVector Wander();
 	FVector Avoidance();
-	void SetPhysicsType();
 
 
 	bool target;											// The subject of the steering behaviour.
@@ -60,17 +40,10 @@ public:
 	BoidWeights weights; 
 	float flockRange;
 
-	ABoidManager* manager;									// Set during spawn
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 private:
-	UStaticMeshComponent* mesh;
-	FVector currentVelocity = FVector::ZeroVector;
-	FVector targetVelocity = FVector::ZeroVector;
-	FVector oldWanderTarget = FVector::ZeroVector;
-	FVector currentWanderTarget = FVector::ZeroVector;
 
 };
